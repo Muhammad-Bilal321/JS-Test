d6c1b3e323e2d1a2744382347f5dfc88755ccc0e
Muhammad-Bilal321 <128062466+Muhammad-Bilal321@users.noreply.github.com>
2023-05-17 17:27:41 +0500

    Add files via upload

---
+++ JS Class Assignment/App.js
@@ -0,0 +1,585 @@
+// Chapter No:1
+
+// 1. Alert these following (individually):
+// alert("First Name: M.Bilal");
+// alert("Last Name: Javed Ahmed");
+// alert("Email: beerusunix72@gmail.com");
+// alert("Phone: 090078601");
+// alert("Password: XXXXXXX");
+
+// 2. Correct this statement: alert"You're learning JavaScript!";
+// alert("You're learning JavaScript!");
+
+// 3. Code an alert statement displaying any message you like.
+// alert("klajdslkasjdlkasjdkla");
+
+// Chapter No:2
+// var priceDiscount;
+
+// var a;
+
+// a = "Bilal";
+
+// var teamName = "Cloud9"
+// alert(teamName);
+
+// var bestMan = "Charlie";
+
+// bestMan = "Elon Musk";
+
+// Chapter 3
+//1. Declare a variable “caseQty”
+// var caseCity;
+
+//2. Assign to the variable caseQty, which has already been declared,
+// the value 144.
+// caseCity = 144;
+
+// 3. Rewrite this statement so the variable can be used in a math
+// operation. var num = "9";
+
+// var num = 9;
+
+// 4. In one statement declare a variable. In a second statement assign
+// it the sum of 2 numbers.
+
+// var sum;
+
+// sum = 5+9;
+
+// 5. What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// Try it yourself.
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal)
+
+// 6. In the first statement declare a variable and assign it a number. In
+// the second statement, change the value of the variable by adding
+// it together with a number.
+
+// var a = 10;
+
+// a = a + 10;
+
+// alert(a)
+
+// Chapter 4
+
+// 1. Correct this statement.
+// var product cost = 3.45;
+// var productcost = 3.45
+
+// 2. Rewrite this using camelCase.
+// var Nameofband;
+
+// var NameOfBand;
+
+// 3. In a single statement declare a legally-named variable and assign a
+// number to it.
+
+// var number = 4;
+
+// 4. Declare a variable that is a combination of your first and last
+// names. Use camelCase.
+
+// 5. List the legal and Illegal Variables.
+
+// Legal Variables
+
+// var myVariable;
+// var _privateVariable;
+// var $specialVariable;
+// var camelCase;
+// var number1;
+
+// Illegal Variables
+
+// var 123number; 
+// var special@var; 
+// var #illegalVar;
+// var if; 
+// var class; 
+
+// Chapter 5 (Math Expression I)
+
+// 1. What is the name and symbol of the arithmetic operator that
+// gives you the remainder when one number is divided by another?
+
+// Modulus = %
+
+// 2. What is the value of num?
+// var num = 20 % 6;
+// Answer Is 2
+
+// 3. In a single statement, declare the variable largeNum and assign it
+// the result of 1,000 multiplied by 2,000.
+
+// var largeNum = 1000 * 2000;
+
+// 4. Assign to a variable the value represented by one variable
+// subtracted from the value represented by another variable
+ 
+// var a = 100;
+// var b = a - 50;
+
+// 5. Assign to a variable the remainder when one number is divided by
+// another. The variable hasn't been declared beforehand. Make up
+// the variable name.
+
+// var a = 15;
+// var b = 3;
+// var c = a % b;
+// console.log(c)
+
+// 6. Code an alert that displays the result of a multiplication on 2
+// numbers.
+
+// alert(5 * 2);
+
+// Chapter 6 (Math Expression II)
+
+// 1. Code a short form of x = x + 1; Use either of the two legal
+// expressions.
+
+// ++x or x++
+
+// 2. If x has a value of 100, what is the fastest way to reduce it to 99
+// with a math expression?
+
+// var x = 100;
+// x = --x;
+// alert(x)
+
+// 3. var x = 50;
+// var y = x++;
+// What is the value of y?
+// var x = 50;
+// var y = x++;
+
+// Value Is 50
+
+// 4. var y = 50;
+// var z = --y;
+// What is the value of z?
+// var y = 50;
+// var z = --y;
+// alert(z)
+// Value is 49
+
+// 5. In a single statement, decrement num and assign its original value
+// to newNum.
+
+// var a = 10;
+// var newNum = a--;
+
+// 6. In a single statement add 1 to a variable and assign its original
+// value to another variable.
+
+// var a = 10;
+// var b = a++;
+
+// 7. Assign a number value to a variable.
+// Increment the variable.
+// Display the new value in an alert
+// var number = 5;
+// number++;
+
+// alert(number);
+
+
+// Chapter 7 (Math Expression III)
+
+// 1. var calculatedNum = 2 + (2 * 6);
+// What is the value of calculatedNum?
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum); // Output: 14
+
+// 2. var calculatedNum = (2 + 2) * 6;
+// What is the value of calculatedNum?
+// var calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum); // Output: 24
+
+// 3. var calculatedNum = (2 + 2) * (4 + 2);
+// What is the value of calculatedNum?
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum); // Output: 24
+
+// 4. var calculatedNum = ((2 + 2) * 4) + 2;
+// What is the value of calculatedNum?
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum); // Output: 18
+
+// Chapter 8 (Concatenating Text Strings)
+
+// 1. var num = "2" + "2";
+// What is the value of num? Include quotation marks.
+// var num = "2" + "2";
+
+// console.log(num) //Output 22
+
+// 2. message = ("Hello," + "Dolly");
+// What is the value of message? (Include the quotation marks.)
+// Alert the statement
+// var message = ("Hello," + "Dolly");
+
+// alert(message)//Output is Hello Dolly
+// 3. alert("33" + 3);
+// What message displays in the alert box?
+// Output is 333
+
+// 4. Write an alert that displays the concatenation of the two parts of
+// "Pakistan Zindabad".
+
+// var a = "Pakistan" + "Zindabad"
+// alert(a) //Output is Pakistan Zindabad
+
+// Chapter 9 (Prompts)
+
+// 1. Code a prompt with the message "Enter first name". The user's
+// response is assigned to firstName.
+
+// prompt("Enter First Name")
+
+// 2. Code a prompt with the message "Country?" and the default
+// answer "China". The user's response is assigned to country.
+
+// prompt("Country?","China")
+
+// 3. Correct this statement
+// var yourName = prompt(Enter Your Name");
+// var yourName = prompt("Enter Your Name");
+
+// 5. Assign strings to two variables. Code a prompt specifying the first
+// variable as the message and the second variable as the default
+// response. Assign the user's response to a third variable.
+
+// var a1 = "Enter your message";
+// var a2 = "Default response";
+
+// var b = prompt(a1, a2);
+
+// 6. Display a prompt, including both a message and a default
+// response.
+// Display the user's response in an alert.
+
+// var a = prompt("");
+
+// alert(a)
+
+// Chapter 10 (if statments)
+
+// 1. var city = "Karachi"
+// if (city = "Karachi") {
+// console.log("The City OF Lights")
+// Correct the above statement:
+// Also try this statement by yourself
+// var city = "Karachi"
+// if (city == "Karachi") {
+// console.log("The City OF Lights")} // Output Karachi
+
+// 2. This is the first line of an if statement:
+// if (x === y) {
+// Complete the statement. If the condition is true, display a box
+// that asks the user value of z? and assign it to another variable.
+// var x = +prompt("Please enter the value of x:");
+// var y = +prompt("Please enter the value of y:");
+// if (x === y) {
+//     var z = prompt("Please enter the value of z:");
+//     }
+//     alert(z)
+
+// 3. Code an if statement that tests if ZipCode is "10010" so, Alert
+// that "Karachi". if not then alert ("Please write correct city")
+
+// var ZipCode = "10010";
+
+// if (ZipCode === "10010") {
+//     alert("Karachi");
+//   } else {
+//     alert("Please write correct city");
+//   }
+
+// 4. Code an if statement. Test whether a variable has a particular
+// numerical value. If so, assign a new value to that variable, as in
+// x = 1;
+
+// if (x == 1) {
+//     x = 2; 
+//   }
+
+// Chapter 11 (Comparison Operators)
+
+
+// 1. Code the first line of an if statement that tests whether one
+// variable is unequal to another. (Use !)
+
+// if (variable1 !== variable2) {
+//     // Code to be executed if the condition is true
+//   }
+
+// 2. Code the first line of an if statement that tests whether the
+// value represented by a variable is greater than or equal to the
+// value represented by another variable.
+
+// if (variable1 >= variable2) {}
+
+// 3. Code an if statement. Test whether a variable is unequal to a
+// particular number. If so, assign a number to that variable.
+
+// if (variable !== 40) {
+//     variable = 10; 
+//   }
+
+// Chapter 12 (if...else and else if statements)
+// var a = +prompt("Enter Num");
+
+// if (a <= 20) {
+//     alert("if statment")
+// } 
+// else if(a <= 50)
+// alert("else if statment")
+// else {
+//     alert("else statment")
+// }
+
+// 2. Write a program using if else and else if statement which take
+// marks from user. And the program will calculate your percentage
+// and give you grade A/C to Your percentage. (MARKSHEET)
+
+
+// var marks = +prompt("Enter your marks:");
+
+
+// var percentage = (marks / 100) * 100;
+
+
+// var grade;
+
+// if (percentage >= 90) {
+//   grade = "A";
+// } else if (percentage >= 80) {
+//   grade = "B";
+// } else if (percentage >= 70) {
+//   grade = "C";
+// } else if (percentage >= 60) {
+//   grade = "D";
+// } else {
+//   grade = "F";
+// }
+
+// console.log("Percentage: " + percentage + "%");
+// console.log("Grade: " + grade);
+
+// 3. This is the if statement that begins the code.
+// if (a === 10) {
+// alert("a is 10");
+// }
+// If a isn't 10, display an alert that says The correct value of a is
+
+// if (a === 10) {
+//     alert("a is 10");
+//     }
+//     else{
+//         alert("The correct value of a is 10");
+//     }
+
+// Chapter 13 (Testing sets of conditions)
+// var a = 10;
+// var b = 10;
+// var c = 20;
+// var d = 20;
+
+// if (a === b && c === d) {
+//    alert("True")
+//   }
+
+//   else{
+//     alert("False")
+//   }
+
+// 2. Code the first line of an if statement that tests whether either or
+// both are true: a has the same value as b or c doesn't have the
+// same value as d.
+// var a = 10;
+// var b = 15;
+// var c = 20;
+// var d = 25;
+
+// if (a === b || c !== d) {
+//    alert("True")
+//   }
+
+//   else{
+//     alert("False")
+//   }
+
+// 3. Code the first line of an if statement that tests whether
+// I. name is either "Hamza" or "Arsalan".
+// II. age is Less than 60.
+// var name = prompt("Enter Name")
+
+// if (name === "Hamza" || name === "Arsalan") {
+//   alert("welcome "+ name);
+// }
+// var age = +prompt("Enter Age")
+
+// if (age < 60) {
+//   alert("Your Age is "+ age)
+// }
+
+// Chapter 14 (If statements nested)
+
+// 1. Code an if statement enclosing a nested if. If password is not
+// empty, then check if password is not greater than 5 , then display
+// an alert that says "Password must be greater than 5" if greater
+// than 5 then Alert "OK".
+
+// if (password !== "") {
+//   if (password.length <= 5) {
+//     alert("Password must be greater than 5.");
+//   } else {
+//     alert("OK");
+//   }
+// }
+
+// 2. Try this statement by yourself
+// if (a === 1) {
+// if (c === "Max") {
+// alert("OK");
+// }
+// }
+
+// var a = 1;
+
+// if (a === 1) {
+//   var c = "Max";
+//   if (c === "Max") {
+//   alert("OK");
+//   }
+//   }
+
+// 4. Declare two variables and assign them the same number value.
+// Test two conditions, using nested if statements. Test whether the
+// first variable equals the second variable and also whether it is less
+// than or equal to the second variable. If the test passes—and it
+// will—display an alert message.
+
+// var a1 = 5;
+// var a2 = 5;
+
+// if (a1 === a2) {
+//   if (a1 <= a2) {
+//     alert("The conditions passed!");
+//   }
+// }
+
+// Chapter 15 (Array I)
+
+// 1. Declare an empty array.
+
+// var a = [];
+
+// 2. Code an array with 1 string element
+// var myArray = ["Hello"];
+
+// 3. var alphabet = ["h","i","j","k"]. Now print the letter “j” in alert
+// using array index
+
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]); // Output: "j"
+
+// 4. var alphabet=["h","i","j","k", “l”,”m”, “n”, “o”]. Find the total
+// length of array.
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// var length = alphabet.length;
+// alert(length); // Output: 8
+
+// Chapter 16 (Array II)
+
+// 1. Code an array with 1 string element.
+// Add an additional element to the array using push.
+
+// var arr = ["string"];
+// arr.push("string2");
+// alert(arr);
+
+// 2. var Alphabet=["h","i","j","k"]
+// Remove the last element from the array Alphabet.
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.pop();
+
+// 3. var Alphabet=["h","i","j","k"]
+// Add a new element, a number, to the end of an array.
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.push(5);
+
+// Chapter 16 (Array III)
+// 1. var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+// Remove the first element of an array.
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift();
+
+// 2. var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+// Add three number elements to the beginning of an array.
+  
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift(1, 2, 3);
+
+// 3. Declare an array with one element.
+// Add a second element to the beginning of the array.
+// Create an alert whose message is the new first element.
+
+// var arr = [1];
+// arr.unshift(2);
+// alert(arr[0]);
+
+// 4. var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+// Insert "L" into the array between "M" and "XL".
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2, 0, "L");
+
+// Chapter 17 - 20 (for Loops)
+
+// 1. Write a statement in which loop is to run 10 times.
+
+// for (var i = 0; i <=9; i++) {
+//   console.log(i) //Output is 10
+  
+// }
+
+// 2. Code the first line of a for loop with the usual counter name, usual
+// starting value, and usual increment. Run it 12 times using <= to
+// specify how many loops.
+// for (var i = 0; i <=11; i++) {
+//   console.log(i) //Output is 12
+  
+// }
+
+// 3. What are the 5 characters missing from this code, excluding any
+// spaces that are missing? Type them in order, with no spaces or
+// commas between them. 
+// for var i = 0 i <= 4 i++
+
+// for (var i = 0; i <= 4; i++){
+
+// }
+
+// 5. Code the first line of a for loop with the usual counter and the
+// usual starting value. Run it 3 times using > to specify how many
+// loops. Decrement it with each iteration.
+
+// for (var  i = 3; i > 0; i--) {
+
+//   console.log(i);
+// }
+
+
+
+
